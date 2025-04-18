953873fb56c53dd6c40068852f81a7f0cd6ec8a1
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-04-18 13:44:30 +0300

    Ajout des cards recettes

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,20 @@
 import {initDropdowns} from "./dropdowns-button.js";
 import {initClearInput} from './cross-clear-input.js';
+import {recipeTemplate} from "./recipe-template.js";
+import {recipes} from "../recipes.js"
 
-document.addEventListener('DOMContentLoaded', function() {
-    initDropdowns();
-})
-
+initDropdowns();
 initClearInput();
+
+const container = document.querySelector(".container-recipes-cards");
+
+recipes.forEach(recipe => {
+    const template = recipeTemplate(recipe);
+    const card = template.getRecipeCardDom();
+    container.appendChild(card);
+});
+
+
+
+
+
---
+++ javascript/recipe-template.js
@@ -0,0 +1,66 @@
+export function recipeTemplate(data) {
+    const {id, image, name, servings, ingredients, time, description, appliance, ustensils} = data;
+
+    const picture = `../assets/json-recipes/${image}`;
+
+    function getRecipeCardDom() {
+        const article = document.createElement("article");
+        article.classList.add("recipe-article");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt",`Image de ${name}`);
+        img.classList.add("recipe-img");
+        
+        const timeTag = document.createElement("span");
+        timeTag.classList.add("recipe-time");
+        timeTag.textContent = `${time}min`;
+
+        article.appendChild(img);
+        article.appendChild(timeTag);
+
+        const content = document.createElement("div");
+        content.classList.add("recipe-content");
+
+
+        const title = document.createElement("h2");
+        title.classList.add("recipe-name");
+        title.textContent = name;
+
+        const subtitleRecipe = document.createElement("h3");
+        subtitleRecipe.classList.add("recipe-subtitle");
+        subtitleRecipe.textContent = "RECETTE";
+
+
+        const pDescription = document.createElement("p");
+        pDescription.classList.add("recipe-description");
+        pDescription.textContent = description;
+
+        const subtitleIngredients = document.createElement("h3");
+        subtitleIngredients.classList.add("recipe-subtitle");
+        subtitleIngredients.textContent = "INGRÉDIENTS"
+
+
+        const ulIngredients = document.createElement("ul");
+        ulIngredients.classList.add("recipe-ingredients");
+
+        ingredients.forEach(ingredient => {
+            const li = document.createElement("li");
+            li.textContent = `${ingredient.ingredient} : ${ingredient.quantity || ""} ${ingredient.unit || ""}`;
+            ulIngredients.appendChild(li);
+        });
+
+
+        content.appendChild(title);
+        content.appendChild(subtitleRecipe);
+        content.appendChild(pDescription);
+        content.appendChild(subtitleIngredients);
+        content.appendChild(ulIngredients);
+
+        article.appendChild(content);
+
+        return article;
+    }
+
+    return {id, image, name, servings, ingredients, time, description, appliance, ustensils, getRecipeCardDom};
+}
