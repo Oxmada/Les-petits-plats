2b2109a4b1d6855868f08a520b7aec75c3053001
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-04-21 17:44:42 +0300

    Ajout algo de recherche fonctionnelle + ajout des filtres des menus déroulants

--- javascript/main.js
+++ javascript/main.js
@@ -8,13 +8,38 @@ initClearInput();
 
 const container = document.querySelector(".container-recipes-cards");
 
-recipes.forEach(recipe => {
-    const template = recipeTemplate(recipe);
-    const card = template.getRecipeCardDom();
-    container.appendChild(card);
-});
+
+displayRecipes(recipes);
+displayFiltersInfo(recipes);
 
 
+function displayRecipes(recipes) {
+    container.innerHTML = "";
+
+    recipes.forEach(recipe => {
+        const template = recipeTemplate(recipe);
+        const card = template.getRecipeCardDom();
+        container.appendChild(card);
+    });
+}
+
+function displayFiltersInfo(recipes) {
+    const ingredientsList = document.querySelector(".ingredients-list");
+    const applianceList = document.querySelector(".appliance-list");
+    const ustensilsList = document.querySelector(".ustensils-list");
+
+    ingredientsList.innerHTML = "";
+    applianceList.innerHTML = "";
+    ustensilsList.innerHTML = "";
+
+    recipes.forEach(recipe => {
+        const template = recipeTemplate(recipe);
+        template.getFiltersInfoDom();
+    });
+}
+
+const searchButton = document.querySelector(".search-button");
+initSearchInput("main-search", searchButton, recipes, displayRecipes);
 
 
 
