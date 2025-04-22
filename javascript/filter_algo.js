b6f2369ffbccd4580db46c32a18dfa0dd0c1ab78
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-04-22 16:53:53 +0300

    Ajout des tags dans l'algo de filtre

--- javascript/filter_algo.js
+++ javascript/filter_algo.js
@@ -30,8 +30,47 @@ export function initFilter(allRecipes, displayRecipes) {
             const selected = item.textContent.toLowerCase();
             if (!activeFilters.includes(selected)) {
                 activeFilters.push(selected);
+                createFilterTag(selected, displayRecipes, allRecipes);
                 applyFilter(displayRecipes, allRecipes);
             }
         });
     });
+}
+
+function createFilterTag(filterName, displayRecipes, allRecipes) {
+    const filtersContainer = document.querySelector(".active-filters");
+
+    const tag = document.createElement("div");
+    tag.className = "filter-tag bg-yellow-400 text-black px-3 py-1 rounded flex items-center gap-2";
+
+    tag.innerHTML = `
+    <span>${filterName}</span>
+    <button class="remove-tag text-black font-bold cursor-pointer">&times;</button>`;
+
+    tag.querySelector(".remove-tag").addEventListener("click", () => {
+        const index = activeFilters.indexOf(filterName);
+        if (index !== -1) activeFilters.splice(index, 1);
+
+        tag.remove();
+
+        const filtered = allRecipes.filter(recipe => {
+            return activeFilters.every(filter => {
+                const inName = recipe.name.toLowerCase().includes(filter);
+                const inDesc = recipe.description.toLowerCase().includes(filter);
+                const inIngredients = recipe.ingredients.some(ing =>
+                    ing.ingredient.toLowerCase().includes(filter)
+                );
+                const inAppliance = recipe.appliance.toLowerCase().includes(filter);
+                const inUstensils = recipe.ustensils.some(u =>
+                    u.toLowerCase().includes(filter)
+                );
+                return inName || inDesc || inIngredients || inAppliance || inUstensils;
+            });
+        });
+
+        displayRecipes(filtered);
+    });
+
+    filtersContainer.appendChild(tag);
+    
 }
